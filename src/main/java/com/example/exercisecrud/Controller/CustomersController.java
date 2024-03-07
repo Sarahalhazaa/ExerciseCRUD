5ed2c77ce6a53cd01b3d4f57450ae694fba4a0ca
sarahalhazaa <Sarahalhazaa2000@gmail.com>
2024-03-07 22:56:18 +0300

    Initial commit

---
+++ src/main/java/com/example/exercisecrud/Controller/CustomersController.java
@@ -0,0 +1,65 @@
+package com.example.exercisecrud.Controller;
+import com.example.exercisecrud.Api.ApiResponse;
+import com.example.exercisecrud.Model.Customers;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+@RestController
+@RequestMapping("/api/v1/Customers")
+public class CustomersController {
+
+
+   ArrayList<Customers> customers = new ArrayList<>();
+
+    @GetMapping("/get")
+    public ArrayList<Customers> getCustomers() {
+        return customers;
+    }
+
+    @PostMapping("/add")
+    public ApiResponse addCustomers(@RequestBody Customers customer) {
+        customers.add(customer);
+        return new ApiResponse("Customers added");
+    }
+
+    @PutMapping("/update/{index}")
+    public  ApiResponse updatedCustomers(@PathVariable int index, @RequestBody Customers customer) {
+       customers.set(index, customer);
+        return new  ApiResponse(" Customers updated ");
+    }
+
+
+    @DeleteMapping("/delete/{index}")
+    public  ApiResponse deletedCustomers(@PathVariable int index) {
+        customers.remove(index);
+        return  new  ApiResponse("Customers Deleted");
+    }
+    @GetMapping("/getdeposit/{id}/{amount}")
+    public ApiResponse deposit(@PathVariable String id,@PathVariable int amount) {
+        for (Customers customer : customers){
+     if (customer.getID().equals(id)){
+         int b = customer.getBalance()+amount;
+         customer.setBalance(b);
+
+         return new  ApiResponse("The money has been deposited");
+     }
+
+     }
+        return new  ApiResponse("No money has been deposited");
+    }
+
+    @GetMapping("/withdraw/{id}/{amount}")
+    public  ApiResponse withdraw(@PathVariable String id,@PathVariable int amount) {
+        for (Customers customer : customers){
+        if (customer.getID().equals(id)){
+            if (amount<= customer.getBalance()){
+                int b = customer.getBalance()-amount;
+                customer.setBalance(b);
+                return new  ApiResponse("The money has been withdrawn");
+
+            }
+        }
+    }
+        return new  ApiResponse("The money has not been withdrawn");
+}
+    }
---
+++ src/main/java/com/example/exercisecrud/Model/Customers.java
@@ -0,0 +1,13 @@
+package com.example.exercisecrud.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customers {
+
+    private String ID ;
+    private String Username ;
+    private int Balance ;
+}
